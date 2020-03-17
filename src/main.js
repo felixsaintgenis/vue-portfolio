ca07918087079d5e1a8eb7f85a7c07112509d1fe
Félix Saint-Genis <saintgenisfelix@gmail.com>
2020-03-17 15:57:14 +0100

    add markdown configuration

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueSimpleMarkdown from 'vue-simple-markdown'
+import 'vue-simple-markdown/dist/vue-simple-markdown.css'
 
 Vue.config.productionTip = false
+Vue.use(VueSimpleMarkdown)
 
 new Vue({
   render: h => h(App),
