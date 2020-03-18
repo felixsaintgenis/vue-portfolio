1d52c35fa6edfb4c1b7762a13c807542ab512cee
Félix Saint-Genis <saintgenisfelix@gmail.com>
2020-03-18 19:00:23 +0100

    create a postCard component for project list

---
+++ src/components/PostCard.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="post-card">
+    <div class="card">
+      <div class="card-image">
+        <figure class="image is-square">
+          <img :src="image" alt="Placeholder image" />
+        </figure>
+      </div>
+      <div class="card-content">
+        <div class="media">
+          <div class="media-content">
+            <p class="title is-4">{{title}}</p>
+          </div>
+        </div>
+        <div class="content">
+          <router-link :to="'/project/'+slug" class="button is-fullwidth">Voir le projet</router-link>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "PostCard",
+  props: {
+    title: String,
+    date: String,
+    snippet: String,
+    image: String,
+    slug: String
+  }
+};
+</script>
